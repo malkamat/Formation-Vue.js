241fcd90ac480175b97c67c9f5a3185c993d3256
malkamat <rimka.matkalo@gmail.com>
2020-09-13 13:23:32 +0200

    Premier TP de la formation : créer une To-Do List

---
+++ src/components/Contenu.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="container mt-5">
+        <h1>Entre des choses à faire.</h1>
+
+        <form action="">
+
+            <div class="form-group">
+
+                <label for="action">Action</label>
+                <input v-model="formData.tache" type="text" id="action" class="form-control">
+
+
+            </div>
+
+            <button @click.prevent="creationItem" class="btn btn-primary mb-3">Créer une nouvelle tâche</button>
+
+        </form>
+
+        <ul>
+            <li :key="index" v-for="(tache , index) in tableauTaches">
+                <item :id="index" :tache="tache" :suppression="suppression"></item>
+            </li>
+        </ul>
+     
+        </div>
+</template>
+
+
+
+
+<script>
+
+import Item from "./Item"
+
+
+export default {
+    name: "Contenu",
+    data() {
+        return {
+           formData: {
+               tache: "",
+
+           },
+           tableauTaches: ["Javascript", "Vue", "Python", "Node"]
+          
+        }
+    },
+    components: {
+        "item": Item,
+
+    },
+    methods: {
+       creationItem: function(){
+           if(this.formData.tache !== "") {
+               this.tableauTaches.push(this.formData.tache)
+           this.formData.tache = ""
+           }
+           
+       },
+       suppression: function(e){
+           this.tableauTaches.splice(e.target.parentNode.id , 1)
+       }
+
+    }, 
+   
+}
+
+
+
+</script>
+
+
+
+
+
+
+<style scoped>
+
+h1{
+margin-top: 100px!important;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="header-top"><p>TP : To-Do Liste</p></div>
+</template>
+
+
+<script>
+
+
+
+export default {
+    name: "Header",
+    data(){
+        return {
+            titre: "Mon titre de base"
+        }
+
+    }
+}
+</script>
+
+<style scoped>
+
+.header-top {
+    position: fixed;
+    top: 0;
+    height: 50px;
+    background: #333333;
+    color: #f1f1f1;
+    width: 100%;
+    text-align: center;
+    line-height: 50px;
+    font-size: 25px;
+
+}
+
+
+
+</style>
